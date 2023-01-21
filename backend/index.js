7235bd8dbdb03ef75f65de3577e59152dd956bb2
francescodist <francesco.dstf@gmail.com>
2023-01-21 02:33:11 +0100

    Init project

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 5000;
+
+app.get("/status", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
